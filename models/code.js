fdfa1920f3ea2c57adfeb572cb4902ea4d7f6c62
Abdul Raheem <59764290+Raheemrana@users.noreply.github.com>
2022-04-02 17:54:02 +0500

    code saving

--- models/code.js
+++ models/code.js
@@ -1,6 +1,13 @@
 const mongoose = require('mongoose')
 const { User } = require("./user");
 const codeSchema = new mongoose.Schema({
+    id: {
+        type: String,
+        unique: true
+    },
+    name: {
+        type: String
+    },
     body: {
         type: String
     },
--- routes/code.js
+++ routes/code.js
@@ -4,10 +4,23 @@ const Code = require("../models/code")
 const verify = require('./verifyToken')
 
 
-router.post("/save",verify, async (req, res) => {
-    console.log('Hello bro')
-    await new Code({body: req.body.code, user_id: req.userid }).save();
+router.post("/save", async (req, res) => {
+    await new Code({ id: req.body.id, body: req.body.code, user_id: req.userid }).save();
     res.status(200).send({ message: "Code Saved!!" });
+});
+
+router.get("/get:id", async (req, res) => {
+
+    console.log(req.params.id)
+    const code = await Code.findOne({id: req.params.id})
+    if (code){
+        console.log('Found')
+        res.status(200).send({ code_name: code.name , code_body: code.body});
+    }
+    else{
+        console.log('Not found')
+        res.status(405).send({ message: 'Invalid Code Id'});
+    }
 });
 
 
